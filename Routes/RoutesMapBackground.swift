53282876dd1d9ac86b0c55f373d046acf789c548
Jesús Daniel Martínez <90727612+LoGebo@users.noreply.github.com>
2025-03-30 05:33:46 -0600

    v1.0

---
+++ Routes/RoutesMapBackground.swift
@@ -0,0 +1,18 @@
+//
+//  RoutesMapBackground.swift
+//  Drop
+//
+//  Created by Jesús Daniel Martínez García on 30/03/25.
+//
+
+import SwiftUI
+
+struct RoutesMapBackground: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RoutesMapBackground()
+}
