8f8343debc259b428511444bb073cf29c082c318
ReneDTapia <renedario.tapia.6@gmail.com>
2025-03-29 18:07:44 -0600

    Initial Commit

---
+++ Drop/DropApp.swift
@@ -0,0 +1,17 @@
+//
+//  DropApp.swift
+//  Drop
+//
+//  Created by Dario on 29/03/25.
+//
+
+import SwiftUI
+
+@main
+struct DropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DropTests/DropTests.swift
@@ -0,0 +1,17 @@
+//
+//  DropTests.swift
+//  DropTests
+//
+//  Created by Dario on 29/03/25.
+//
+
+import Testing
+@testable import Drop
+
+struct DropTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
